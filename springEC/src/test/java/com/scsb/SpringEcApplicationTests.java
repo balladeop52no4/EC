26fcd4ed5ab850e3ec55d92e5f548bf23d25eba0
balladeop52no4 <voyagerzark@gmail.com>
2023-04-28 21:35:40 +0800

    Spring boot project init

---
+++ springEC/src/test/java/com/scsb/SpringEcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
